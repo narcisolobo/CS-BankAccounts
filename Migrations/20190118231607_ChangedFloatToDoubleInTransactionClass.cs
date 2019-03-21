e11d6121bb442d67c87d1877d5db1e557ec84870
Narciso Lobo <narcisolobo@gmail.com>
2019-03-21 10:43:13 -0500

    removed appsettings.json

---
+++ Migrations/20190118231607_ChangedFloatToDoubleInTransactionClass.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BankAccounts.Migrations
+{
+    public partial class ChangedFloatToDoubleInTransactionClass : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Migrations/20190119001528_AddedStringFields.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BankAccounts.Migrations
+{
+    public partial class AddedStringFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AmountString",
+                table: "Transactions",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedAtString",
+                table: "Transactions",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AmountString",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAtString",
+                table: "Transactions");
+        }
+    }
+}
